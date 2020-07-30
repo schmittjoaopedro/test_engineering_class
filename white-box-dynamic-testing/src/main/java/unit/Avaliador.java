a0e1fc4def303149d5d1f816a95dafe31d9e4f59
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2020-07-30 16:03:25 -0300

    step 8 - add more tests with lists

--- white-box-dynamic-testing/src/main/java/unit/Avaliador.java
+++ white-box-dynamic-testing/src/main/java/unit/Avaliador.java
@@ -1,19 +1,37 @@
 package unit;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Avaliador {
 
     private double maiorDeTodos = Double.NEGATIVE_INFINITY;
     private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private List<Lance> maiores;
 
     public void avalia(Leilao leilao) {
         for (Lance lance : leilao.getLances()) {
             if (lance.getValor() > maiorDeTodos) {
                 maiorDeTodos = lance.getValor();
             }
-            if(lance.getValor() < menorDeTodos) {
+            if (lance.getValor() < menorDeTodos) {
                 menorDeTodos = lance.getValor();
             }
         }
+        pegaOsMaioresNo(leilao);
+    }
+
+    private void pegaOsMaioresNo(Leilao leilao) {
+        maiores = new ArrayList<>(leilao.getLances());
+        Collections.sort(maiores, Comparator
+                .comparing(Lance::getValor).reversed());
+        maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
+    }
+
+    public List<Lance> getTresMaiores() {
+        return maiores;
     }
 
     public double getMaiorLance() {
--- white-box-dynamic-testing/src/test/java/unit/AvaliadorTest.java
+++ white-box-dynamic-testing/src/test/java/unit/AvaliadorTest.java
@@ -2,9 +2,29 @@ package unit;
 
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class AvaliadorTest {
+
+    @Test
+    public void deveEncontrarOsTresMaioresLances() {
+        Usuario joao = new Usuario("Joao");
+        Usuario maria = new Usuario("Maria");
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+        leilao.propoe(new Lance(joao, 100.0));
+        leilao.propoe(new Lance(maria, 200.0));
+        leilao.propoe(new Lance(joao, 300.0));
+        leilao.propoe(new Lance(maria, 400.0));
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+        List<Lance> maiores = leiloeiro.getTresMaiores();
+        assertEquals(3, maiores.size());
+        assertEquals(400, maiores.get(0).getValor(), 0.00001);
+        assertEquals(300, maiores.get(1).getValor(), 0.00001);
+        assertEquals(200, maiores.get(2).getValor(), 0.00001);
+    }
 
     @Test
     public void lancesEmOrdemCrescenteTest() {
