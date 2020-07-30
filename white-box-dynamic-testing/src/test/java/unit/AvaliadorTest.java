e36f505b3abc3c2e5702b286d8417b41708422ca
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2020-07-30 15:12:08 -0300

    step 6 - add more equivalence classes

--- white-box-dynamic-testing/src/test/java/unit/AvaliadorTest.java
+++ white-box-dynamic-testing/src/test/java/unit/AvaliadorTest.java
@@ -21,4 +21,44 @@ public class AvaliadorTest {
         Assert.assertEquals(250.0, leiloeiro.getMenorLance(), 0.001);
     }
 
+    @Test
+    public void lancesEmOrdemDecrescenteTest() {
+        Usuario joao = new Usuario("Joao");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+        leilao.propoe(new Lance(jose, 400.0));
+        leilao.propoe(new Lance(joao, 300.0));
+        leilao.propoe(new Lance(maria, 250.0));
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+        Assert.assertEquals(400.0, leiloeiro.getMaiorLance(), 0.001);
+        Assert.assertEquals(250.0, leiloeiro.getMenorLance(), 0.001);
+    }
+
+    @Test
+    public void lancesSemOrdemEspecificaTest() {
+        Usuario joao = new Usuario("Joao");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+        leilao.propoe(new Lance(joao, 300.0));
+        leilao.propoe(new Lance(jose, 400.0));
+        leilao.propoe(new Lance(maria, 250.0));
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+        Assert.assertEquals(400.0, leiloeiro.getMaiorLance(), 0.001);
+        Assert.assertEquals(250.0, leiloeiro.getMenorLance(), 0.001);
+    }
+
+    @Test
+    public void lancesUnicoTest() {
+        Usuario joao = new Usuario("Joao");
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+        leilao.propoe(new Lance(joao, 300.0));
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+        Assert.assertEquals(300.0, leiloeiro.getMaiorLance(), 0.0001);
+        Assert.assertEquals(300.0, leiloeiro.getMenorLance(), 0.0001);
+    }
 }
