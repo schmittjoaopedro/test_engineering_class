605fd858ebe83af31a6fb341050dc8a8876639d0
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2020-07-30 13:14:50 -0300

    Step 3 - simulating client bug

--- white-box-dynamic-testing/src/main/java/unit/TesteDoAvaliador.java
+++ white-box-dynamic-testing/src/main/java/unit/TesteDoAvaliador.java
@@ -7,9 +7,9 @@ public class TesteDoAvaliador {
         Usuario jose = new Usuario("José");
         Usuario maria = new Usuario("Maria");
         Leilao leilao = new Leilao("Playstation 3 Novo");
+        leilao.propoe(new Lance(maria, 250.0));
         leilao.propoe(new Lance(joao, 300.0));
         leilao.propoe(new Lance(jose, 400.0));
-        leilao.propoe(new Lance(maria, 250.0));
         Avaliador leiloeiro = new Avaliador();
         leiloeiro.avalia(leilao);
         // imprime 400.0
