9daef5eaace3efeac868991e445235dfde5cb799
João Pedro Schmitt <schmittjoaopedro@gmail.com>
2020-11-14 15:25:25 -0300

    remove unnecessary waiting

--- e-commerce-cypress/cypress/integration/case-5.spec.js
+++ e-commerce-cypress/cypress/integration/case-5.spec.js
@@ -7,8 +7,6 @@ context('Case 5', () => {
     it('Ao selecionar um produto específico, verificar que existem diferentes formas de pagamento disponíveis.', () => {
         cy.get("input[id='h_search-input']").type('smartphone');
         cy.get("button[id='h_search-btn']").click();
-        // espera 1.5 segundos para que a página começar a ser atualizada conforme a ordem definida
-        cy.wait(1500);
         // verifica que existem 25 elementos com o atributo 'to' contendo o texto "pfm_carac=smartphone" 
         cy.get("a[to*='pfm_carac=smartphone'] > div:first").click();
         // clicar em "mais formas de pagamaneto"
--- e-commerce-cypress/cypress/integration/case-6.spec.js
+++ e-commerce-cypress/cypress/integration/case-6.spec.js
@@ -7,8 +7,6 @@ context('Case 6', () => {
     it('Ao selecionar um produto específico, executar o calculo do frete para entrega. Verificar que ao inserir um CEP é retornado o custo', () => {
         cy.get("input[id='h_search-input']").type('smartphone');
         cy.get("button[id='h_search-btn']").click();
-        // espera 1.5 segundos para que a página começar a ser atualizada conforme a ordem definida
-        cy.wait(1500);
         // verifica que existem 25 elementos com o atributo 'to' contendo o texto "pfm_carac=smartphone" 
         cy.get("a[to*='pfm_carac=smartphone'] > div:first").click();
         // obter campo do frete
